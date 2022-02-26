0cf4baeb9965480eb2dc5db695714a40ac852a3d
Zelei Regina <zeleiregina@gmail.com>
2022-02-26 17:34:58 +0100

    Add project files.

---
+++ Levente02.26.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {   string path = @"C:\Users\User\Downloads\e_inffor_17maj_fl\valaszok.txt";
+            if(!File.Exists(path))
+            {
+                Console.WriteLine("Nem talhato fajl");
+            }else
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                    Console.WriteLine(line);
+                string helyes = lines[0];
+
+                string[] valaszok = new string[lines.Length-1];
+                string[] nevek = new string[lines.Length - 1];
+
+                for(int i=1; i<lines.Length; i++)
+                {
+                    int szokozhelye = lines[i].IndexOf(" ");
+
+                    valaszok[i] = lines[i].Substring(szokozhelye+1);
+                    nevek[i] = lines[i].Substring(0, szokozhelye);
+                    Console.WriteLine(szokozhelye);
+                    Console.WriteLine(valaszok[i]);
+                    Console.WriteLine(nevek[i]);
+                }
+            }
+            
+
+        }
+    }
+}
